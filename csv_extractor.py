e7bb9365f2e473da7116b1d21776a2d3772bf34e
Derick Tseng <dericktseng@tutanota.com>
2023-05-01 21:39:13 -0700

    fix diode plots

--- csv_extractor.py
+++ csv_extractor.py
@@ -3,11 +3,11 @@ import glob
 
 def write_csv_data(infile, outfile):
     with open(infile) as csvfile:
-        rdr = list(csv.reader(csvfile))[109:]
+        rdr = list(csv.reader(csvfile))[104:]
     with open(outfile, 'w+') as csvfile:
         wtr = csv.writer(csvfile, delimiter=' ')
         wtr.writerows(rdr)
     return
 
-for file in glob.glob('*MOSFET*.csv'):
-    write_csv_data(file, file + '_out.txt')
+for file in glob.glob('*.csv'):
+    write_csv_data(file, file[:-4] + '_out.txt')
